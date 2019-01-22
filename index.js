50cd71429c8080b7deb33f19a90c8ab163653b6c
mymrtt <mymrtt@gmail.com>
2019-01-22 15:15:38 -0200

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+const container = document.getElementById("container");
+const titleElement = document.createElement("h1");
+titleElement.innerHTML = "OFF/ON";
+
+const containerTitle = document.getElementById("title").appendChild(titleElement);
